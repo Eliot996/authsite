dcaeea925e67fa333b5fef64991618fc94b29c36
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-26 19:32:47 +0200

    changed to initial value

--- auth-site-backend/databases/createDatabase.js
+++ auth-site-backend/databases/createDatabase.js
@@ -3,38 +3,20 @@ import db from "./connection.js";
 const isDeleteMode = process.argv.findIndex((argument) => argument === "delete_mode") === -1 ? false : true;
 
 if (isDeleteMode) {
-    db.exec(`DROP TABLE planets;`);
-    db.exec(`DROP TABLE people;`);
+    db.exec(`DROP TABLE users;`);
 }
 
 // (DDL)
 db.exec(`
-CREATE TABLE IF NOT EXISTS planets (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    size FLOAT,
-    is_habitable BOOLEAN NOT NULL
-);
-`);
-
-db.exec(`
-CREATE TABLE IF NOT EXISTS people (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    planet_id INTEGER,
-    FOREIGN KEY (planet_id) REFERENCES planets (id)
+    email TEXT NOT NULL,
+    password varchar(64) NOT NULL
 );
 `);
 
 // Seeding (DML)
 if (isDeleteMode) {
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Mercury', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Venus', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Earth', True);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Mars', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Jupiter', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Saturn', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Uranus', False);`);
-    db.exec(`INSERT INTO planets (name, is_habitable) VALUES ('Neptune', False);`);
+    db.exec(`INSERT INTO users (email, password) VALUES ('email', 'password');`);
 }
 
