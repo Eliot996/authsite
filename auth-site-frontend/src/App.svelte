0c61ba04808ef20f83dd4afae5aea28f771cef05
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-27 18:59:18 +0200

    added logout functionality

--- auth-site-frontend/src/App.svelte
+++ auth-site-frontend/src/App.svelte
@@ -2,12 +2,21 @@
   import { Router, Link, Route } from "svelte-navigator";
   import Counter from './lib/Counter.svelte';
   import Home from './pages/home.svelte';
+  import { BASE_URL, USER } from './store/globalsStore.js';
+
+  export function logout() {
+    fetch($BASE_URL + '/logout');
+    $USER = undefined;
+  }
 </script>
 
 <Router>
   <nav>
     <Link to="/" class="link-button">Home</Link>
     <Link to="/counter" class="link-button">Counter</Link>
+    {#if $USER}
+      <span class="link-button" on:click={logout}>logout</span>
+    {/if}
   </nav>
   <Route path="/">
     <div class="card">
@@ -17,8 +26,7 @@
   <Route path="/counter">
     <div class="card">
       <Counter />
-    </div>
-    
+    </div> 
   </Route>
 </Router>
 
