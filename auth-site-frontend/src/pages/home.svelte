8bdd062123b86c927e8be186d981eb1f4cffc81a
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-27 18:35:33 +0200

    added page for login

---
+++ auth-site-frontend/src/pages/home.svelte
@@ -0,0 +1,47 @@
+<script>
+    import { BASE_URL, USER} from '../store/globalsStore.js';
+    import { useNavigate } from 'svelte-navigator';
+    const navigate = useNavigate();
+
+    import toast, { Toaster } from 'svelte-french-toast';
+
+    async function login() {
+        const loginObject = {email: document.getElementById('email-input').value,
+                             password: document.getElementById('password-input').value}
+        
+        const response = await fetch($BASE_URL + '/login', {
+            method: 'POST',
+            credentials: 'include', 
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(loginObject)
+        });
+        
+        if (response.status !== 200) {
+            toast.error("Wrong password or email");
+            return;
+        }
+        
+        const result = await response.json();
+        $USER = result.userID;
+        navigate('/counter');
+    }
+</script>
+
+<Toaster />
+
+<h1>Welcome</h1>
+To AuthSite
+
+<p>
+    Please sign in before continuing
+</p>
+
+<div>
+    <div>
+        <label>Email: <input type="text"id='email-input'/></label><br> 
+        <label>Password: <input type="password" id="password-input"></label><br>
+    </div>
+    <button on:click={login}>login</button>
+</div>
