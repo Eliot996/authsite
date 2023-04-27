049fb4488177bee42cbb522c0340a7fe18ec87be
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-27 10:31:42 +0200

    changed how the app responses and added login

--- auth-site-backend/routes/userRouter.js
+++ auth-site-backend/routes/userRouter.js
@@ -4,5 +4,25 @@ const router = Router();
 
 router.post("/create", async (req, res) => {
     const userID = await users.create(req.body.email, req.body.password);
-    res.send("Hello, " + userID);
+
+    if (userID) {
+        req.session.userID = userID;
+        res.send({ userID: userID });
+    } else {
+        res.sendStatus(300);
+    }
 });
+
+router.post("/login", async (req, res) => {
+    const userID = await users.authenticate(req.body.email, req.body.password);
+
+    if (userID) {
+        req.session.userID = userID;
+        res.send({ userID: userID });
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+
+export default router;
