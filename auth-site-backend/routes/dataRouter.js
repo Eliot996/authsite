cb498f5286c87e3daee1b57f39396b59a724dcbd
Eliot996 <90563339+Eliot996@users.noreply.github.com>
2023-04-27 19:43:05 +0200

    added some secret data

---
+++ auth-site-backend/routes/dataRouter.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const router = Router();
+
+router.get('/data', (req, res) => {
+    if (req.session.userID) {
+        res.send({ secret: 42 });
+    }
+    res.status(401).send({ message: 'Try logging in first!'})
+});
